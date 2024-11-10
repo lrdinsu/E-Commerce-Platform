d8ddf9037b6b030124c08e84c7ad780f93d15b98
lilia <143701282+lrdinsu@users.noreply.github.com>
2024-11-10 14:57:45 -0800

    Initial commit

---
+++ src/main/java/com/lrdinsu/ecommerceplatform/EcommerceplatformApplication.java
@@ -0,0 +1,13 @@
+package com.lrdinsu.ecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrdinsu/ecommerceplatform/EcommerceplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrdinsu.ecommerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
